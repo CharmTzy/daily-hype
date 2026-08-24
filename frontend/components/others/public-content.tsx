a90f453c36d07b1cc5f54458053f926114d08cf2
CharmTzy <wai71308@gmail.com>
2026-08-24 09:45:20 +0800

    fix

--- frontend/components/others/public-content.tsx
+++ frontend/components/others/public-content.tsx
@@ -1,20 +1,14 @@
 "use client";
-import { useAppState } from "@/app/app-provider";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 export default function PublicContent({ children }: {
     children: React.ReactNode;
 }) {
-    const { headerCanLoad } = useAppState();
     return (
-        <>
-            {headerCanLoad && (
-                <div className="min-h-screen bg-white text-slate-950">
-                    <Header />
-                    <main className="min-h-[calc(100vh-81px)]">{children}</main>
-                    <Footer />
-                </div>
-            )}
-        </>
+        <div className="min-h-screen bg-white text-slate-950">
+            <Header />
+            <main className="min-h-[calc(100vh-81px)]">{children}</main>
+            <Footer />
+        </div>
     );
 }
